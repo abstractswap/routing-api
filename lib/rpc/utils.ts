dda40eadd701d4e3c1589c1f7586133113cae2d6
msvstj <savastijonok@gmail.com>
2025-10-14 20:55:55 +0300

    fix: add missing config

--- lib/rpc/utils.ts
+++ lib/rpc/utils.ts
@@ -18,6 +18,8 @@ export function chainIdToNetworkName(networkId: ChainId): string {
       return 'avalanchec'
     case ChainId.BASE:
       return 'base'
+    case ChainId.STABLE_TESTNET:
+      return 'stable-testnet'
     default:
       return 'ethereum'
   }
@@ -140,6 +142,9 @@ export function generateProviderUrl(key: string, value: string): string {
       return tokens[0]
     }
     case 'WEB3_RPC_GATEWAY_60808': {
+      return tokens[0]
+    }
+    case 'WEB3_RPC_GATEWAY_2201': {
       return tokens[0]
     }
     case 'WEB3_RPC_GATEWAY_7560': {
